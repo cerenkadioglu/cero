28c0fea2dc8daadfea6674d5c9147f2071d4d393
ckadioglu <152319942+cerenkadioglu@users.noreply.github.com>
2024-11-29 15:07:46 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,36 @@
+// Giriş yapma işlemi
+async function loginUser(event) {
+    event.preventDefault(); // Formun varsayılan davranışını engelle
+
+    // Form alanlarını oku
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    // API'ye veri gönder
+    try {
+        const response = await fetch('http://192.168.15.253:5208/api/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            alert('Giriş başarılı! Hoş geldiniz, ' + data.username);
+        } else {
+            const errorData = await response.json();
+            alert('Giriş başarısız: ' + errorData.message);
+        }
+    } catch (error) {
+        console.error('Hata:', error);
+        alert('yapabilirsin ceren fighting !!');
+    }
+}
+
+// Giriş yapma butonunu dinle
+document.getElementById('loginForm').addEventListener('submit', loginUser);
